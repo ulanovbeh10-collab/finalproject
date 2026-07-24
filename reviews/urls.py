fa11ff8a189dabc6b049409d5a1fee349c9539bc
ulanovbeh10-collab <ulanovbeh10@gmail.com>
2026-07-24 18:30:47 +0600

    a

--- reviews/urls.py
+++ reviews/urls.py
@@ -4,8 +4,8 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('dish/<int:pk>/', views.dish_detail, name='dish_detail'),
+    path('dish/add/', views.add_dish, name='add_dish'),
     path('profile/', views.profile, name='profile'),
-    path('add/', views.add_dish, name='add_dish'),
     path('register/', views.register_view, name='register'),
     path('login/', views.login_view, name='login'),
     path('logout/', views.logout_view, name='logout'),
